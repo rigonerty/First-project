4df7b6c39ed833bf8413f48bcff2653bf4b67c2c
rigonerty <112642804+rigonerty@users.noreply.github.com>
2022-09-23 22:16:50 +0300

    Update mainScript.js

--- firstProject/mainScript.js
+++ firstProject/mainScript.js
@@ -96,8 +96,8 @@ theme.addEventListener("click", function () {
         root.style.setProperty("--a-hover", "#333333")
         root.style.setProperty("--a-active", "#202020")
         root.style.setProperty("--color-header", "black")
-        theme.src = "/My-projects/firtsProject/img/icon/moon.png"
-        logo.src = "/My-projects/firtsProject/img/logo/logo2.gif"
+        theme.src = "/My-projects/firstProject/img/icon/moon.png"
+        logo.src = "/My-projects/firstProject/img/logo/logo2.gif"
         statusTheme = false
     } else {
         html.style.backgroundColor = rootStyle.getPropertyValue("--white")
@@ -108,8 +108,8 @@ theme.addEventListener("click", function () {
         root.style.setProperty("--background-color", "rgb(245,245,245)")
         root.style.setProperty("--color-header", "white")
         statusTheme = true
-        theme.src = "/My-projects/firtsProject/img/icon/sun.png"
-        logo.src = "/My-projects/firtsProject/img/logo/logo1.gif"
+        theme.src = "/My-projects/firstProject/img/icon/sun.png"
+        logo.src = "/My-projects/firstProject/img/logo/logo1.gif"
     }
 
 })
