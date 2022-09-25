7e1d80a46da9fcb569d01862ed0fcefd4abf40c1
rigonerty <112642804+rigonerty@users.noreply.github.com>
2022-09-25 19:42:28 +0300

    Add files via upload

--- firstProject/homeScript.js
+++ firstProject/homeScript.js
@@ -1,8 +1,8 @@
 
+
 const rightBtnSlider = document.querySelector(".rightBtn")
 const leftBtnSlider = document.querySelector(".leftBtn")
 const slider1 = document.querySelector(".slider1s")
-const slider1Chiled = document.getElementsByClassName("sliderChild")
 let slider1current = 0;
 let slider1MoveStatus = false
 const slider1s = document.querySelector(".slider1")
@@ -12,10 +12,11 @@ const sliderText = document.querySelectorAll(".sliderText")
 let maxLenghtSlider = 300
 let sliderHeadL = 50
 let sliderTextL = 700
+const slider1Chiled = []
 const slider2 = document.querySelector(".slider2s")
 const slider2s = document.querySelector(".slider2")
 let currentXSlider2 = 0;
-const slider2Child = document.querySelectorAll(".slider2Child")
+const slider2Child = []
 const slider2H = document.querySelectorAll(".slider2H")
 const slider2P = document.querySelectorAll(".slider2P")
 const listH = document.querySelectorAll(".listH")
@@ -77,14 +78,22 @@ leftBtnSlider.addEventListener("click", function () {
     }
 })
 
-
 slider1s.onmousedown = function (e) {
+    const a = document.querySelectorAll(".sliderChild > a")
+    for (let i = 0; i < a.length; i++) {
+        a[i].onclick = function () { return true }
+    }
     let slider1Width = slider1s.offsetWidth
     let clientX1 = e.pageX;
     slider1.style.transition = "none"
+    currentX = 0
     document.onmousemove = function (e) {
+        for (let i = 0; i < a.length; i++) {
+            a[i].onclick = function (){ return false}
+        }
         let clientX2 = e.pageX;
         currentX = clientX2 - clientX1
+        
         slider1.style.transform = "translate(" + (-slider1current * slider1Width + currentX) + "px)"
     }
     document.onmouseup = function () {
@@ -121,7 +130,13 @@ slider1s.addEventListener("touchstart", function(e){
     let slider1Width = slider1s.offsetWidth
     let clientX1 = e.changedTouches[0].pageX;
     slider1.style.transition = "none"
+    currentX = 0
+    const a = document.querySelectorAll(".sliderChild > a")
+    for (let i = 0; i < a.length; i++) {
+        a[i].onclick = function () { return true }
+    }
     document.ontouchmove = function (e) {
+        for (let i = 0; i < a.length; i++) { a[i].onclick = function () { return false } }
         let clientX2 = e.changedTouches[0].pageX;
         currentX = clientX2 - clientX1
         slider1.style.transform = "translate(" + (-slider1current * slider1Width + currentX) + "px)"
@@ -176,7 +191,14 @@ slider2s.onmousedown = function (e) {
     let slider2Width = content.offsetWidth;
     let currentSlider2 = 0
     let clientX = e.pageX;
+    const a = document.querySelectorAll(".slider2Child > a")
+    for (let i = 0; i < a.length; i++) {
+        a[i].onclick = function () { return true }
+    }
     document.onmousemove = function (e) {
+        for (let i = 0; i < a.length; i++) {
+            a[i].onclick = function () { return false }
+        }
         let clientX1 = e.pageX;
         currentSlider2 = clientX1 - clientX
         if (currentSlider2 + currentXSlider2 > 0) {
@@ -197,15 +219,26 @@ slider2s.onmousedown = function (e) {
         document.onmouseup = null;
     }
 }
+slider2.ondragstart = function () {
+    return false;
+};
+slider2s.ondragstart = function () {
+    return false;
+};
 let currentSlider2 = 0
 slider2s.addEventListener( "touchstart",function (e) {
     e.preventDefault()
     let slider2Width = content.offsetWidth;
     currentSlider2 = 0
     let clientX = e.changedTouches[0].pageX;
+    const a = document.querySelectorAll(".slider2Child > a")
+    for (let i = 0; i < a.length; i++) {
+        a[i].onclick = function () { return true }
+    }
     document.ontouchmove = function (e) {
         let clientX1 = e.changedTouches[0].pageX;
         currentSlider2 = clientX1 - clientX
+        for (let i = 0; i < a.length; i++) { a[i].onclick = function () { return false } }
         if (currentSlider2 + currentXSlider2 > 0) {
             currentSlider2 = 0
             currentXSlider2 = 0
@@ -237,6 +270,7 @@ fetch("main.json").then(function (response) {
 }).then(function (json) {
     blobImgList(json)
     blobImgSlider1(json)
+    blobImgSlider2(json)
 })
 
 
@@ -264,7 +298,7 @@ function ShowList(blob,product){
     div.appendChild(p)
     a.appendChild(image)
     a.appendChild(div)
-    a.href = `network/manga/${product.name.replace(" ","")}.html`
+    a.href = `/network/manga/${product.name.replace(" ","")}.html`
     divChild.appendChild(a)
     if (leftCountList < 5){
         leftList.appendChild(divChild)
@@ -275,7 +309,7 @@ function ShowList(blob,product){
 }
 function blobImgList(products) {
     for(const product of products){
-        const url = `img/manga/cover/${product.url}`
+        const url = `/img/manga/cover/${product.url}`
         fetch(url).then(function (response) {
             if (!response.ok) {
                 throw new Error(`HTTP error: ${response.status}`)
@@ -291,7 +325,7 @@ function blobImgList(products) {
 }
 function blobImgSlider1(products) {
     for (const product of products) {
-        const url = `img/manga/banner/${product.banner}`
+        const url = `/img/manga/banner/${product.banner}`
         fetch(url).then(function (response) {
             if (!response.ok) {
                 throw new Error(`HTTP error: ${response.status}`)
@@ -319,7 +353,7 @@ function ShowSlider1(blob, product){
     image.alt = product.name
     h2.innerHTML = maxWordsTable1(product.name, sliderHeadL)
     p.innerHTML = maxWordsTable1(product.description, sliderTextL)
-    a.href = `/My-projects/firstProject/network/manga/${product.name.replace(" ", "")}.html`
+    a.href = `/network/manga/${product.name.replace(" ","")}.html`
     divImg.appendChild(image)
     h2.classList.add("sliderHead")
     p.classList.add("sliderText")
@@ -332,17 +366,50 @@ function ShowSlider1(blob, product){
     a.appendChild(divText)
     divChild.appendChild(a)
     slider1.appendChild(divChild)
+    slider1Chiled.push(1)
+}
+function blobImgSlider2(products){
+    for(const product of products){
+        const url = `/img/manga/cover/${product.url}`
+        fetch(url).then(function (response) {
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`)
+            } else {
+                return response.blob()
+            }
+        }).then(function (blob) {
+            ShowSlider2(blob, product)
+        })
+    }
+}
+function  ShowSlider2(blob, product) {
+    const urlObject = URL.createObjectURL(blob)
+    const image = document.createElement("img")
+    const div = document.createElement("div")
+    const h2 = document.createElement("h2")
+    const p = document.createElement("p")
+    const divChild = document.createElement("div");
+    const a = document.createElement("a");
+    divChild.classList.add("slider2Child")
+    div.classList.add("slider2Text")
+    h2.classList.add("slider2H")
+    p.classList.add("slider2P")
+    h2.innerHTML = maxWordsTable1(product.name, 25)
+    p.innerHTML = maxWordsTable1(product.description, 200)
+    a.href = `/network/manga/${product.name.replace(" ", "")}.html`
+    image.src = urlObject
+    div.appendChild(h2)
+    div.appendChild(p)
+    a.appendChild(image)
+    a.appendChild(div)
+    divChild.appendChild(a)
+    slider2.appendChild(divChild)
+    slider2Child.push(1)
 }
 
 
 
 
-
-
-
-
-for (let i = 0; i < slider2H.length; i++) { slider2H[i].innerHTML = maxWordsTable(slider2H[i], 25) }
-for (let i = 0; i < slider2P.length; i++) { slider2P[i].innerHTML = maxWordsTable(slider2P[i], 200) }
 function maxWordsTable(text, n) {
     let textP = text.innerHTML
     if (textP.length > n) {
@@ -357,3 +424,5 @@ function maxWordsTable1(text, n) {
     }
     return textP;
 }
+
+
