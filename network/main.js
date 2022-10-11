be2a21579edc8237172c670cef701f99634006cf
rigonerty <112642804+rigonerty@users.noreply.github.com>
2022-10-11 17:02:53 +0300

    Add files via upload

---
+++ network/main.js
@@ -0,0 +1,207 @@
+const titleBodyButton = document.querySelectorAll(".titleBodyButton")
+const titleBodyChild = document.querySelectorAll(".titleBodyChild")
+const titleH2 = document.querySelector(".titleIhfo > h2")
+const commentTextArea = document.querySelector(".TitleCommentsHead > textarea")
+let commentSend = document.querySelectorAll(".Send")
+
+const TitleCommentsContent = document.querySelector(".TitleCommentsContent")
+const titleHeadImg = document.querySelector(".headTitle > img")
+const titleInfo = document.querySelector(".titleIhfo")
+const titleInfoH2 = document.querySelector(".titleIhfo > h2")
+const title = document.querySelector("title")
+const backImage = document.querySelector(".backImage")
+const descriptionTitle = document.querySelector(".descriptionTitle > p")
+const scoreT = document.querySelector("#scoreT")
+const chaptersT = document.querySelector("#chaptersT")
+const browseT = document.querySelector("#browseT")
+const statusT = document.querySelector("#statusT")
+
+
+commentSend[0].onclick = function () {
+    commentContain(TitleCommentsContent, 0)
+}
+
+
+
+
+commentTextArea.setAttribute('style', 'height:' + (33) + 'px;overflow-y:hidden;');
+commentTextArea.addEventListener("input", OnInput, false);
+function OnInput() {
+    this.style.height = (this.scrollHeight) + 'px';
+}
+function onLoad(){
+    let commentsAnswer = document.querySelectorAll(".answerComm")
+    for (const commentAnswer of commentsAnswer){
+        commentAnswer.onclick = function(){
+            if (!commentAnswer.parentNode.parentNode.childNodes[3].classList.contains("TextAnswer")){
+                const div = document.createElement("div")
+                const textarea = document.createElement("textarea")
+                const button = document.createElement("button")
+                textarea.setAttribute('style', 'height:' + (33) + 'px;overflow-y:hidden;');
+                textarea.addEventListener("input", OnInput, false);
+                button.innerHTML = "Send"
+                button.classList.add("commentSend")
+                commentSend = document.querySelectorAll(".Send")
+                div.append(textarea)
+                div.append(button)
+                div.classList.add("TextAnswer")
+                commentAnswer.parentNode.after(div)
+                button.onclick = function () {
+                    commentContain(commentAnswer.parentNode.parentNode, 1)
+                }
+                document.onclick = function (e) {
+                    const withinDiv = e.composedPath().includes(div)
+                    const withincommentAnswer = e.composedPath().includes(commentAnswer)
+                    if (!withinDiv && !withincommentAnswer){
+                        for(const textAns of document.querySelectorAll(".TextAnswer")){
+                            textAns.remove()
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
+function commentContain(where, i){
+    const img = document.createElement("img")
+    const div1 = document.createElement("div")
+    const div2 = document.createElement("div")
+    const div3 = document.createElement("div")
+    const p = document.createElement("p")
+    const span = document.createElement("span")
+    const button1 = document.createElement("button")
+    const button2 = document.createElement("button")
+    const h3 = document.createElement("h3")
+    const div4 = document.createElement("div")
+    div1.classList.add("comment")
+    span.innerHTML = `${new Date().getDate()}.${new Date().getMonth()}.${new Date().getFullYear()} / ${new Date().getHours()}:${new Date().getMinutes()}`
+    button1.innerHTML = "answer"
+    button1.classList.add("answerComm")
+    button2.innerHTML = "complain"
+    h3.innerHTML = "NAME"
+    div3.classList.add("divButtons")
+    div2.appendChild(img)
+    div2.appendChild(h3)
+    div2.appendChild(span)
+    div3.appendChild(button1)
+    div3.appendChild(button2)
+    div1.appendChild(div2)
+    div1.appendChild(p)
+    div1.appendChild(div3)
+    div1.appendChild(div4)
+    div4.style.display = "block"
+    div1.style.display = "block"
+    if (!i == 0){
+        p.innerHTML = where.childNodes[3].firstChild.value
+        where.childNodes[3].remove()
+        where.childNodes[3].prepend(div1)
+    }else{
+        p.innerHTML = commentTextArea.value
+        commentTextArea.value = ""
+        commentTextArea.style.height = "33px"
+        where.prepend(div1)
+        div1.style.borderLeft = "1px solid transparent"
+    }
+    onLoad()
+}
+
+fetch("/main.json").then(function (response) {
+    return response.json()
+}).then(function (json) {
+    blobImgHead(json)
+    blobImgBackHead(json)
+    Chapters(json)
+})
+
+function blobImgHead(products) {
+    for (const product of products) {
+        if (title.innerHTML == product.name) {
+            let url = `/img/manga/cover/${product.url}`
+            fetch(url).then(function (response) {
+                if (!response.ok) {
+                    throw new Error(`HTTP error: ${response.status}`)
+                } else {
+                    response.blob().then(function (blob) {
+                        showBlobImg(blob, product)
+                    })
+                }
+
+            })
+        }
+    }
+}
+function showBlobImg(blob, product) {
+    const urlObject = URL.createObjectURL(blob)
+    titleHeadImg.src = urlObject
+}
+function blobImgBackHead(products) {
+    for (const product of products) {
+        if (title.innerHTML == product.name) {
+            let url = `/img/manga/banner/${product.banner}`
+            fetch(url).then(function (response) {
+                if (!response.ok) {
+                    throw new Error(`HTTP error: ${response.status}`)
+                } else {
+                    response.blob().then(function (blob) {
+                        showBlobImgBack(blob, product)
+                    })
+                }
+
+            })
+        }
+    }
+}
+function showBlobImgBack(blob, product) {
+    const urlObject = URL.createObjectURL(blob)
+    backImage.style.background = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${urlObject})`
+    backImage.style.backgroundSize = "cover";
+    backImage.style.backgroundPosition = "0% 25%"
+    descriptionTitle.innerHTML = product.description
+    scoreT.innerHTML = product.score;
+    browseT.innerHTML = product.browse;
+    chaptersT.innerHTML = product.chapters;
+    statusT.innerHTML = product.status
+    titleInfoH2.innerHTML = product.name
+    titleInfoH2.title = product.name
+}
+function Chapters(products){
+    for (const product of products) {
+        if (title.innerHTML == product.name) {
+            for(const chapter of product.listChapters){
+                ChaptersLoad(chapter)
+            }
+        }
+    }
+}
+function ChaptersLoad(chapter){
+    if (chapter.name != "THIS IS PAGE DON'T EXIST!!"){
+        const a = document.createElement("a")
+        const h4 = document.createElement("h4")
+        const span = document.createElement("span")
+        a.href = `/network/manga/${title.innerHTML.replaceAll(" ", "")}/Vol1/Chapter${chapter.number}/Chapter${chapter.number}.html`
+        h4.innerHTML = `Chapter: ${chapter.number}`
+        span.innerHTML = chapter.name
+        a.append(h4)
+        a.append(span)
+        titleBodyChild[1].append(a)
+    }
+    
+}
+titleBodyChild[1].style.display = "none"; titleBodyButton[1].classList.remove("active")
+titleBodyChild[2].style.display = "none"; titleBodyButton[2].classList.remove("active")
+for (let i = 0; i < 3; i++) {
+    titleBodyButton[i].onclick = function () {
+        for (let t = 0; t < 3; t++) { titleBodyChild[t].style.display = "none"; titleBodyButton[t].classList.remove("active") }
+        titleBodyChild[i].style.display = "block"
+        titleBodyButton[i].classList.add("active")
+    }
+}
+titleH2.innerHTML = maxWordsTable(titleH2, 38)
+function maxWordsTable(text, n) {
+    let textP = text.innerHTML
+    if (textP.length > n) {
+        textP = textP.substring(0, n) + "...";
+    }
+    return textP;
+}
---
+++ network/manga/script.js
@@ -0,0 +1,373 @@
+
+const NameTitle = document.querySelector(".Name")
+const title = document.querySelector("title")
+NameTitle.innerHTML = `<a href="../../${title.innerHTML.replaceAll(" ", "")}.html">${title.innerHTML}</a>`
+const chapterContent = document.querySelector(".ChapterContent")
+const setting = document.querySelector(".setting") 
+let arrayChapter = []
+let chapterContentImg = document.querySelectorAll(".ChapterContent > img")
+const settingBlock = document.querySelector(".settingBlock")
+const settingWidth = document.querySelector("#settingWidth")
+const settingWidthNumber = document.querySelector("#settingWidthNumber")
+let Json;
+const settingNameTitle = document.querySelector("#settingNameTitle")
+const settingNameChapter = document.querySelector("#settingNameChapter")
+const settingNumberPage = document.querySelector("#settingNumberPage")
+const styleReading = document.querySelector(".styleReading")
+const settingChapterCount = document.querySelector("#settingChapterCount")
+const closeSetting = document.querySelector("#closeSetting")
+const ButtonsChapterContent = document.querySelector("#ButtonsChapterContent")
+const ButtonsChapterContentR = document.querySelector("#ButtonsChapterContentR")
+const ButtonsChapterContentL = document.querySelector("#ButtonsChapterContentL")
+const settingNumberPageCount = document.querySelector("#settingNumberPageCount")
+let countStyleReading = 0
+let countStyleImg = 0
+const settingNumberPageDiv = document.querySelector('#settingNumberPageDiv')
+const settingNumberPageDivDiv = document.querySelector('#settingNumberPageDiv > div')
+ButtonsChapterContent.style.display = "none"
+const settingChapterCountUl = document.querySelector("#settingChapterCountUl")
+const settingChapterCountDiv = document.querySelector("#settingChapterCountDiv")
+let settingNumberPageCountLi = document.querySelectorAll("#settingNumberPageCount > li")
+fetch("/main.json").then(function (response) {
+    if(!response.ok){
+        throw new Error(`HTTP error: ${response.status}`)
+    }else{
+        response.json().then(function (json) {
+            chapterLoader(json)
+            Json = json
+            for (const product of Json) {
+                if (product.name == title.innerHTML) {
+                    Json = product;
+                    settingNameTitle.innerHTML = Json.name
+                    if (Json.listChapters[0].name == "THIS IS PAGE DON'T EXIST!!"){
+                        for (let i = 1; i < Json.listChapters.length; i++ ){
+                            const li = document.createElement("li");
+                            const a = document.createElement("a");
+                            a.innerHTML = "Ch." + Json.listChapters[i].number
+                            a.href = `../Chapter${Json.listChapters[i].number}/Chapter${Json.listChapters[i].number}.html`
+                            li.append(a)
+                            settingChapterCountUl.append(li)
+                    }}
+                    else{
+                        for (let i = 0; i < Json.listChapters.length; i++ ){
+                            const li = document.createElement("li");
+                            const a = document.createElement("a");
+                            a.innerHTML = "Ch." + Json.listChapters[i].number
+                            a.href = `../Chapter${Json.listChapters[i].number}/Chapter${Json.listChapters[i].number}.html`
+                            li.append(a)
+                            settingChapterCountUl.append(li)
+                    }
+                    }
+                    settingNameChapter.innerHTML = Json.listChapters[Href(document.location.href).replaceAll(".html", "").replaceAll("Chapter", "")].name
+                    settingChapterCount.innerHTML = "Ch." + Json.listChapters[Href(document.location.href).replaceAll(".html", "").replaceAll("Chapter", "")].number
+                    
+                }
+            }
+        })
+    }
+    
+})
+
+//3 = 4 - x; 
+
+let CountScrollPage = 0
+let arrayCount = []
+window.addEventListener("scroll", function(){
+    if (countStyleReading == 0){
+        arrayCount = []
+        for (const img of chapterContentImg) {
+            arrayCount.push(img.getBoundingClientRect().top)
+            if (img.getBoundingClientRect().top < 0){
+                arrayCount.pop(img.getBoundingClientRect().top)
+            }
+        }
+        settingNumberPage.innerHTML = `${chapterContentImg.length - arrayCount.length}/${chapterContentImg.length}`
+    }
+    
+})
+
+
+settingWidth.max = html.offsetWidth - (html.offsetWidth / 100) * 4
+settingWidth.value = html.offsetWidth - (html.offsetWidth / 100) * 4
+settingWidthNumber.innerHTML = settingWidth.value
+settingWidth.addEventListener("input", function () {
+    settingWidthNumber.innerHTML = settingWidth.value
+    for (const img of chapterContentImg) {
+        img.style.width = settingWidth.value + "px"
+    }
+})
+
+
+
+function chapterLoader(products){
+    for(const product of products){
+        if(product.name == title.innerHTML){
+            for (let i = 0; i < product.listChapters[Href(document.location.href).replaceAll(".html", "").replaceAll("Chapter", "")].count; i++){
+                arrayChapter.push((fetch(`/network/manga/${product.name.replaceAll(" ", "")}/Vol1/${Href(document.location.href).replaceAll(".html", "")}/${(i + 1).toString().padStart(3,"0")}.jpg`).then(function (response){
+                        if(!response.ok){
+                            
+                            throw new Error(`HTTP error: ${response.status}`)
+                        }
+                        else{return response.blob()}
+                }).then(function (blob) {
+                    return blob
+                })))
+                
+            }
+            Promise.allSettled(arrayChapter).then(function(results){
+                for(const result of results){
+                    const urlObject = URL.createObjectURL(result.value)
+                    const img = document.createElement("img");
+                    img.src = urlObject;
+                    chapterContent.append(img)
+                    chapterContentImg = document.querySelectorAll(".ChapterContent > img")
+                    img.style.width = html.offsetWidth - (html.offsetWidth / 100) * 4 + "px"
+                    settingNumberPage.innerHTML = `0/${chapterContentImg.length}`
+                }
+                for (let i = 0; i < chapterContentImg.length; i++) {
+                    const li = document.createElement("li")
+                    li.innerHTML = i + 1
+                    settingNumberPageCount.append(li)
+                }
+                settingNumberPageCountLi = document.querySelectorAll("#settingNumberPageCount > li")
+                settingNumberPageCountLi.forEach((t,l) =>{
+                    t.addEventListener("click", function () {
+                        settingNumberPage.innerHTML = `${l + 1}/${chapterContentImg.length}`
+                        countStyleImg = l
+                        if (countStyleReading == 1) {
+                            countStyleImg = l
+                            chapterContentImg.forEach((x, y) => {
+                                if (y == countStyleImg) {
+                                    x.style.display = "block"
+                                } else {
+                                    x.style.display = "none"
+                                }
+                                window.scrollTo(0, 0)
+                            })
+                        }else{
+                            window.scrollTo(0,0)
+                            window.scrollTo(0, chapterContentImg[l].getBoundingClientRect().y + 1)
+                        }
+                    })
+                })
+
+            }).then(function(){
+                
+            })
+        }
+    }
+}
+
+
+
+
+
+
+function Href(href){
+    let currentHref = href.split("/")
+    return currentHref[currentHref.length - 1]
+}
+
+
+setting.style.background = "url(/img/icon/Orion_setting.svg)"
+setting.style.backgroundSize = "25px 25px"
+setting.style.backgroundRepeat = "no-repeat"
+setting.style.backgroundPosition = "center"
+
+
+
+window.addEventListener("resize", function () {
+    settingWidth.max = html.offsetWidth - (html.offsetWidth / 100) * 4
+})
+
+theme.addEventListener("click", function () {
+    if (statusTheme) {
+        setting.style.background = "url(/img/icon/Orion_setting.svg)";
+        setting.style.backgroundSize = "25px 25px"
+        setting.style.backgroundRepeat = "no-repeat"
+        setting.style.backgroundPosition = "center"
+        styleReading.style.filter = "invert(0)"
+        closeSetting.style.filter = "invert(0)"
+        closeSetting.style.bordeColor = "var(--red)"
+        closeSetting.onmousemove = function () {
+            closeSetting.style.borderColor = "rgb(185,0,0)"
+        }
+        closeSetting.onmouseout = function () {
+            closeSetting.style.borderColor = "transparent"
+        }
+        root.style.setProperty("--backOnBack", "rgb(221,221,221)")
+        root.style.setProperty("--a-hoverB", "rgb(199, 199, 199)")
+        root.style.setProperty("--a-activeB", "rgb(175, 175, 175)")
+    } else {
+        setting.style.background = "url(/img/icon/Orion_settingWhite.svg)";
+        setting.style.backgroundSize = "25px 25px"
+        setting.style.backgroundRepeat = "no-repeat"
+        setting.style.backgroundPosition = "center"
+        styleReading.style.filter = "invert(100%)"
+        closeSetting.style.filter = "invert(1)"
+        closeSetting.onmousemove = function(){
+            closeSetting.style.borderColor = "#46ffff"
+        }
+        closeSetting.onmouseout = function () {
+            closeSetting.style.borderColor = "transparent"
+        }
+        root.style.setProperty("--backOnBack", "rgb(26,26,26)")
+        root.style.setProperty("--a-hoverB", "rgb(22,22,22)")
+        root.style.setProperty("--a-activeB", "rgb(12,12,12)")
+    }
+
+})
+
+
+
+document.addEventListener("click", function (e) {
+    const withinSearch = e.composedPath().includes(searchBlock);
+    const withinSearchInput = e.composedPath().includes(inputSearch)
+    const withinSearchImg = e.composedPath().includes(loopSearch)
+    const withinSettingBlock = e.composedPath().includes(settingBlock)
+    const withinSetting = e.composedPath().includes(setting)
+    const withinSettingNumberPageDivDiv = e.composedPath().includes(settingNumberPageDivDiv)
+    const wthinSettingChapterCount = e.composedPath().includes(settingChapterCount)
+    if (!withinSearch && (!withinSearchInput && !withinSearchImg) && searchstatus) {
+        settingBlock.style.position = "fixed"
+        settingBlock.style.display = "block"
+        settingBlock.style.top = "0px"
+        settingBlock.style.height = "500px"
+    }
+    if(!withinSettingBlock && !withinSetting){
+        settingBlock.style.right = "-250px"
+    }
+    if(!withinSettingNumberPageDivDiv){
+        settingNumberPageDiv.style.height = "46px"
+        settingNumberPageDivDivStatus = true
+        settingNumberPageDivDiv.classList.remove("rotate")
+    } if (!wthinSettingChapterCount){
+        settingChapterCountDiv.style.height = "40px"
+        settingChapterCountStatus = true
+        settingChapterCount.classList.remove("rotate")
+    }
+})
+document.addEventListener("keydown", function (e) {
+    if (e.keyCode == 27) {
+        settingBlock.style.position = "fixed"
+
+    }
+})
+
+closeSetting.addEventListener("click", function(){
+    settingBlock.style.right = "-250px"
+})
+setting.addEventListener("click", function(){
+    settingBlock.style.right = "0"
+})
+
+
+styleReading.addEventListener("click", function(){
+    countStyleReading++
+    if (countStyleReading > 1) {
+        countStyleReading = 0
+    }
+    styleReading.style.width = "100%"
+    styleReading.style.height = "41px"
+    if(countStyleReading == 0){
+        styleReading.style.width = "100%"
+        styleReading.style.height = "48px"
+        chapterContentImg.forEach((x, y) => {    
+            x.style.display = "block" 
+            
+        })
+        window.scrollTo(0, 0)
+        window.scrollTo(0, chapterContentImg[countStyleImg].getBoundingClientRect().y + 1)
+        ButtonsChapterContent.style.display = "none"
+    }
+    if (countStyleReading == 1) {
+        chapterContentImg.forEach((x,y)=>{
+            if (y == countStyleImg) {
+                x.style.display = "block"
+            }else{
+                x.style.display = "none"
+            }
+        })
+        ButtonsChapterContent.style.display = "flex"
+    }
+    styleReading.style.background = `url(/img/icon/styleReading${countStyleReading}.png)`
+    styleReading.style.backgroundSize =  "contain";
+    styleReading.style.backgroundRepeat =  "no-repeat";
+    styleReading.style.backgroundPosition = "center";
+})
+
+ButtonsChapterContentL.addEventListener("click", function(){
+    countStyleImg--
+    if (countStyleImg < 0){
+        countStyleImg = 0
+    }
+    chapterContentImg.forEach((x, y) => {
+        if (y == countStyleImg) {
+            x.style.display = "block"
+        } else {
+            x.style.display = "none"
+        }
+    })
+    window.scrollTo(0,0)
+    settingNumberPage.innerHTML = `${countStyleImg}/${chapterContentImg.length}`
+    
+})
+ButtonsChapterContentR.addEventListener("click", function () {
+    countStyleImg++
+    if (countStyleImg > chapterContentImg.length) {
+        countStyleImg = chapterContentImg.length
+    }
+    chapterContentImg.forEach((x, y) => {
+        if (y == countStyleImg) {
+            x.style.display = "block"
+        } else {
+            x.style.display = "none"
+        }
+    })
+    window.scrollTo(0, 0)
+    settingNumberPage.innerHTML = `${countStyleImg + 1}/${chapterContentImg.length}`
+})
+
+navClose.addEventListener("click", function () {
+    settingWidth.max = html.offsetWidth - (html.offsetWidth / 100) * 4
+    
+
+})
+navShow.addEventListener("click", function () {
+    for( const img of chapterContentImg){
+        console.log(html.offsetWidth - (html.offsetWidth / 100) * 4)
+        if (img.offsetWidth + 200 > html.offsetWidth - (html.offsetWidth / 100) * 4){
+            img.style.width = img.offsetWidth- 230 + "px" 
+            settingWidth.max = html.offsetWidth - (html.offsetWidth / 100) * 4 - 230
+        }
+        else{
+            img.style.width = img.offsetWidth + "px"
+        }
+    }
+    
+})
+let settingNumberPageDivDivStatus = true
+settingNumberPageDivDiv.addEventListener("click", function(){
+    if(settingNumberPageDivDivStatus){
+        settingNumberPageDiv.style.height = `${4 * 28 + 8 + 45}px`
+        settingNumberPageDivDivStatus = false
+        settingNumberPageDivDiv.classList.add("rotate")
+        
+    }else{
+        settingNumberPageDiv.style.height = "46px"
+        settingNumberPageDivDivStatus = true
+        settingNumberPageDivDiv.classList.remove("rotate")
+    }
+})
+let settingChapterCountStatus = true
+settingChapterCount.addEventListener("click", function () {
+    if (settingChapterCountStatus) {
+        settingChapterCountDiv.style.height = `${4*32 + 41}px`
+        settingChapterCountStatus = false
+        settingChapterCount.classList.add("rotate")
+    } else {
+        settingChapterCountDiv.style.height = "40px"
+        settingChapterCountStatus = true
+        settingChapterCount.classList.remove("rotate")
+    }
+})
